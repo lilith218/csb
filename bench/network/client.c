9fdc4cb88095c42f35b5fb4c50e2249ec83598ff
Lilith Oberhauser <lilith.oberhauser@huawei.com>
2026-02-27 17:16:13 +0200

    wip
    Signed-off-by: Lilith Oberhauser <lilith.oberhauser@huawei.com>

--- bench/network/client.c
+++ bench/network/client.c
@@ -143,6 +143,7 @@ readwrite(struct epoll_event *ev, struct epoll_st *est)
     int fd              = d->fd;
     int r               = 0;
     if (ev->events & (EPOLLHUP | EPOLLERR)) {
+        printf("EPOLLHUP or EPOLLERR\n");
         unregister(d, est);
         return;
     }
@@ -150,19 +151,26 @@ readwrite(struct epoll_event *ev, struct epoll_st *est)
     if (ev->events & EPOLLOUT) {
         assert(eops[d->step].is_write);
         r = send(fd, &sbuf[0], eops[d->step].sz, 0);
+        if(r > 0)
+            printf("[send] %s\n", sbuf);
     } else if (ev->events & EPOLLIN) {
         assert(!eops[d->step].is_write);
         r = recv(fd, &sbuf[0], eops[d->step].sz, 0);
+        if(r > 0)
+        printf("[recv] %s\n", sbuf);
     }
     if (r == -1) {
         unregister(d, est);
         return;
     }
+    printf("return of send/recv %d\n", r);
     advance_step(d);
     if (0 && d->step == 0) {
+        printf("Completed a full cycle of operations.\n");
         unregister(d, est);
         return;
     } else {
+        printf("config_wait\n");
         config_wait(d, est);
     }
 }
@@ -188,7 +196,8 @@ main(int argc, char *argv[])
     char *program   = NULL;
     bool use_ipv6   = false;
     int opt;
-    while ((opt = getopt(argc, argv, "6h:p:P:")) != -1) {
+    bool retry_on_fail = false; /* retry if the connection fails till it succeeds*/
+    while ((opt = getopt(argc, argv, "6h:p:P:R")) != -1) {
         switch (opt) {
             case 'h':
                 host = optarg;
@@ -204,6 +213,9 @@ main(int argc, char *argv[])
                 use_ipv6 = true;
             case 'P':
                 program = optarg;
+                break;
+            case 'R':
+                retry_on_fail = true;
                 break;
             default: /* '?' */
                 usage(argv[0]);
@@ -270,6 +282,7 @@ main(int argc, char *argv[])
     struct epoll_st est = {};
     est.fd              = epoll_create1(0);
     if (est.fd == -1) {
+        fprintf(stderr, "epoll_create1 failed!\n");
         return 1;
     }
 
@@ -284,11 +297,18 @@ main(int argc, char *argv[])
 
         int csock = socket(use_ipv6 ? AF_INET6 : AF_INET, SOCK_STREAM, 0);
         if (csock == -1) {
+            fprintf(stderr, "socket failed!\n");
             return 1;
         }
         d->fd = csock;
 
-        if (connect(csock, serv_addr, addr_size) == -1) {
+        int connect_ret = 0;
+        do {
+            connect_ret = connect(csock, serv_addr, addr_size);
+        } while(retry_on_fail && connect_ret != 0);
+
+        if (connect_ret == -1) {
+            fprintf(stderr, "connect failed!\n");
             return 2;
         }
 
@@ -297,21 +317,26 @@ main(int argc, char *argv[])
         ev.events   = d->last_epoll;
         ev.data.ptr = d;
         if (epoll_ctl(est.fd, EPOLL_CTL_ADD, csock, &ev) == -1) {
+            fprintf(stderr, "epoll_ctl failed!\n");
             return -1;
         }
         est.nconn++;
     }
 
+    printf("%zu connections established.\n", est.nconn);
     while (est.nconn) {
         struct epoll_event evs[MAX_EVS];
+        printf("waiting ... \n");
         int nfds = epoll_wait(est.fd, &evs[0], MAX_EVS, -1);
         if (nfds == -1) {
             perror("epoll_wait");
             continue;
         }
+        printf("waiting ... nfds: %d \n", nfds);
         for (int i = 0; i < nfds; i++) {
             readwrite(&evs[i], &est);
         }
     }
+    printf("Exiting.\n");
     return 0;
 }
