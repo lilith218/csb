a33b668ffe304c9c93d659fe30dbe4f3f65eb627
Lilith Oberhauser <lilith.oberhauser@huawei.com>
2026-03-09 12:45:31 +0200

    update
    Signed-off-by: Lilith Oberhauser <lilith.oberhauser@huawei.com>

--- scripts/plugins/generic-network-client.sh
+++ scripts/plugins/generic-network-client.sh
@@ -12,12 +12,12 @@ CLIENT_PATH=$2
 META_PATH=$3
 IP="127.0.0.0"
 
-#META_STRING=$(cat "$META_PATH")
-# grep everything after = and between quotes!
+# grep everything after `=` and between quotes `"..."`
 # WARNING multiple sequences "", "", "" are not handled
 META_STRING=$(grep '^SERVER_SEQ=' $META_PATH | sed 's/^SERVER_SEQ="\([^"]*\)"/\1/')
 echo $META_STRING
 # TODO: check if meta string is empty
 # TODO: create an analogous case for CLIENT_SEQ
-
+# TODO: make it decide whether to launch a client or a server based on
+# the available sequence
 ${CLIENT_PATH} -R -h ${IP} -p${PORT} -P${META_STRING}
