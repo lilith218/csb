d973cf4680e0cff7eaa5bf70a81f98fff8b693ad
Lilith Oberhauser <lilith.oberhauser@huawei.com>
2026-03-11 17:56:16 +0200

    WIP...
    Signed-off-by: Lilith Oberhauser <lilith.oberhauser@huawei.com>

--- bm-generator/test.sh
+++ bm-generator/test.sh
@@ -2,26 +2,28 @@
 # Copyright (C) Huawei Technologies Co., Ltd. 2026. All rights reserved.
 # SPDX-License-Identifier: MIT
 set -e
-
+source helper/bm-generator-lib.sh
 STRACE_LOG="ls_strace.log"
 APP="ls -la /dev"
-../scripts/plugins/collect_strace.sh ${STRACE_LOG} ${APP}
-echo "STEP#0: Initializing ..."
-./00_init.sh
-echo "STEP#1: Building ..."
-./01_build.sh
-echo "STEP#2: Parsing ${STRACE_LOG} ..."
-./02_parse.sh ${STRACE_LOG}
-echo "STEP#3: Extracting ..."
-./03_extract.sh
-echo "STEP#4: Preparing ..."
-./04_prepare.sh
-echo "STEP#5: Generating ..."
-./05_generate.sh
+#../scripts/plugins/collect_strace.sh ${STRACE_LOG} ${APP}
+# echo "STEP#0: Initializing ..."
+# ./00_init.sh
+# echo "STEP#1: Building ..."
+# ./01_build.sh
+# echo "STEP#2: Parsing ${STRACE_LOG} ..."
+# ./02_parse.sh ${STRACE_LOG}
+# echo "STEP#3: Extracting ..."
+# ./03_extract.sh
+# echo "STEP#4: Preparing ..."
+# ./04_prepare.sh
+# echo "STEP#5: Generating ..."
+# ./05_generate.sh
 
 echo "STEP#6: Build and test ..."
 cd ../build
+set -x
 # We only want to build related targets not everything.
-find ../bench/targets/gen-ws -name "min_ls_*.h" | grep -v syz | sed 's/.*\/\(min_ls_.*\)\.h/gen-ws_\1/' | xargs make
+WS=$(get_workspace_dir)
+find ../bench/targets/$WS -name "min_ls_*.h" | grep -v syz | sed "s/.*\/\(min_ls_.*\)\.h/${WS}_\1/" | xargs make
 # We run only related tests
 ctest -R min_ls_* --output-on-failure
